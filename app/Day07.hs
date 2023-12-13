107b03aafdfc82b75d2d8f2064fd795b216da0cf
linh <plh22.ic.ac.uk>
2023-12-13 03:09:25 +0000

    Fixed Day07

--- app/Day07.hs
+++ app/Day07.hs
@@ -1,29 +1,24 @@
 module Day07 where
 import Prelude hiding (split)
-import Utils (split, printDay, replace, splitLine)
+import Utils (split, printDay, replace, splitLine, sdecimal)
 import Data.List (nub, sortBy)
 import qualified Data.Map as M
+import Text.Gigaparsec
+import Text.Gigaparsec.Combinator
+import Text.Gigaparsec.Char
 
 data HandType = HighCard | OnePair | TwoPairs | ThreeOAK | FullHouse | FourOAK | FiveOAK
-   deriving (Show, Read, Eq, Ord, Enum)
-data CardVal = Joker | Two | Three | Four | Five | Six | Seven | Eight | Nine | T | J | Q | K | A
    deriving (Show, Read, Eq, Ord)
-type Cards = String
+data CardVal = Joker | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+   deriving (Show, Read, Eq, Ord)
+type Cards = [CardVal]
 type Hand = (Cards, Int)
 type HandAndType = (HandType, Hand)
-
-cardMap :: M.Map Char CardVal
-cardMap = M.fromList (zip ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'] 
-              [Two, Three, Four, Five, Six, Seven, Eight, Nine, T, J, Q, K, A])
-              
-cardMapJ :: M.Map Char CardVal
-cardMapJ = M.fromList (zip ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'] 
-              [Two, Three, Four, Five, Six, Seven, Eight, Nine, T, Joker, Q, K, A])
 
 getType :: Hand -> Bool -> HandAndType
 getType cb@(c, b) j = (findType c j, cb)
 
-findType :: String -> Bool -> HandType 
+findType :: Cards -> Bool -> HandType 
 findType c j
    | uniquel == 5 = HighCard
    | uniquel == 4 = OnePair
@@ -34,38 +29,58 @@ findType c j
          unique = nub c
          uniquel' = length unique
          uniquel
-            | j || noj = uniquel'
+            | j || (jcount' == 0) = uniquel'
             | otherwise = max 1 (uniquel' - 1)
-         jcount' = length (filter (== 'J') c)
-         noj = jcount' == 0
+         jcount' = length (filter (== Joker) c)
          jcount = if j then 0 else jcount'
          cards3 = filter (==3) (map (\s -> length (filter (==s) c) +jcount) unique)
          cards4 = filter (==4) (map (\s -> length (filter (==s) c) +jcount) unique)
 
-sortHands :: M.Map Char CardVal -> HandAndType -> HandAndType -> Ordering  
-sortHands m (t1, (c1, _)) (t2, (c2, _))
-   | t1 < t2 = LT
-   | t1 > t2 = GT
-   | otherwise = sortCards m c1 c2
-
-sortCards :: M.Map Char CardVal -> Cards -> Cards -> Ordering
-sortCards _ [] [] = EQ
-sortCards m (c:c1) (c':c2) 
-   | m M.! c < m M.! c' = LT
-   | m M.! c > m M.! c' = GT
-   | otherwise = sortCards m c1 c2
+sortHands :: HandAndType -> HandAndType -> Ordering  
+sortHands (t1, (c1, _)) (t2, (c2, _))
+   | t1 == t2 = compare c1 c2
+   | otherwise = compare t1 t2
  
+parseCard :: Parsec CardVal
+parseCard = choice 
+   [Two <$ string "2",
+    Three <$ string "3",
+    Four <$ string "4",
+    Five <$ string "5",
+    Six <$ string "6",
+    Seven <$ string "7",
+    Eight <$ string "8",
+    Nine <$ string "9",
+    Ten <$ string "T",
+    Queen <$ string "Q",
+    King <$ string "K",
+    Ace <$ string "A"
+   ]
+
+parsep1 :: Parsec [CardVal]
+parsep1 = do
+   many (parseCard <|> (Jack <$ string "J"))
+
+parsep2 :: Parsec [CardVal]
+parsep2 = do
+   many (parseCard <|> (Joker <$ string "J"))
+
+parseHand :: Parsec [CardVal] -> Parsec Hand
+parseHand parsecard = do
+   cards <- parsecard <* whitespaces
+   bid <- sdecimal <* whitespaces
+   return (cards, bid)
+
 solve :: IO ()
 solve = do
    inp <- readFile "inputs/Day07.inp"
-   let lines = splitLine inp
-   let cardbid = map (`split` ' ') lines 
-   let cbs = map (\cb -> (head cb, read (cb !! 1) :: Int )) cardbid
-   let handtypes = map (`getType` True) cbs 
-   let sorthands = sortBy (sortHands cardMap) handtypes
-   let ans = sum (zipWith (\(_, (_, b)) i -> b*i) sorthands [1..])
+   let (Success res) = parse @String (many (parseHand parsep1)) inp
+   let handtypes1 = map (`getType` True) res
+   let sorthands1 = sortBy sortHands handtypes1
+   let ans = sum (zipWith (\(_, (_, b)) i -> b*i) sorthands1 [1..])
 
-   let handtypesj = map (`getType` False) cbs 
-   let sorthandsj = sortBy (sortHands cardMapJ) handtypesj
-   let ans2 = sum (zipWith (\(_, (_, b)) i -> b*i) sorthandsj [1..])
+   let (Success res2) = parse @String (many (parseHand parsep2)) inp
+   let handtypes2 = map (`getType` False) res2
+   let sorthands2 = sortBy sortHands handtypes2
+   let ans2 = sum (zipWith (\(_, (_, b)) i -> b*i) sorthands2 [1..])
    printDay 7 ans ans2
